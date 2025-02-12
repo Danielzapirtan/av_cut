48518753571c8f4891216361dc6dd1b788abf3f9
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-02-12 14:31:12 +0000

    m

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def cut_media(input_path, output_path, start_time, end_time=None):
         with VideoFileClip(input_path) as video:
             if end_time is None:
                 end_time = video.duration
-            cut_video = video.subclip(start_time, end_time)
+            cut_video = video.subclipped(start_time, end_time)
             cut_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
     else:
         with AudioFileClip(input_path) as audio:
             if end_time is None:
                 end_time = audio.duration
-            cut_audio = audio.subclip(start_time, end_time)
+            cut_audio = audio.subclipped(start_time, end_time)
             cut_audio.write_audiofile(output_path)
 
 def download_from_url(url):
