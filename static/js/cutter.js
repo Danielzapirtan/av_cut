d104a010ca171e5be59e46b3b71d59b87d8e547a
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-04 22:47:39 +0300

    Tue Aug  4 22:47:39 EEST 2026

--- static/js/cutter.js
+++ static/js/cutter.js
@@ -173,8 +173,8 @@ class MediaCutter {
     updateReverseTimeline() {
         if (this.duration === 0) return;
         
-	const startPercent = this.startHandle.style.left.slice(0,2);
-	const endPercent = this.endHandle.style.left.slice(0,2);
+	const startPercent = this.startHandle.style.left.split('%')[0];
+	const endPercent = this.endHandle.style.left.split('%')[0];
 	this.startTime = startPercent * this.duration / 100;
 	this.endTime = endPercent * this.duration / 100;
     }
