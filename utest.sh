f2b1833f1645c614aa28d7facebc797938b2dab3
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-04 23:26:55 +0300

    Tue Aug  4 23:26:55 EEST 2026

---
+++ utest.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+VER=3.14
+
+if test -z $VIRTUAL_ENV; then
+	test -d venv || python$VER -m venv venv
+	source venv/bin/activate
+	export VIRTUAL_ENV
+fi
+apt install ffmpeg yt-dlp
+pip install -r requirements.txt
+python$VER app.py
+
