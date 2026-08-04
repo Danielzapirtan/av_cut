555aad90b28acd6ab1ab096cf90d514c1fd74962
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-04 20:10:08 +0300

    Tue Aug  4 20:10:08 EEST 2026

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ def cleanup():
 
 if __name__ == '__main__':
     app.secret_key = Config.SECRET_KEY
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5040)
